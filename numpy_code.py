b616991fc2c1bcba0e3b08dd4501403e5c3d275e
Kalyani Vankoju <kalyanivankoju07@gmail.com>
2026-09-25 14:44:25 +0530

    Initial Commit

---
+++ numpy_code.py
@@ -0,0 +1,7 @@
+import numpy as np
+
+scores = np.array([78, 85, 92, 67, 88], dtype=float)
+
+print("NumPy import successful!")
+print("Mean score:", scores.mean())
+print("Highest score:", scores.max())
